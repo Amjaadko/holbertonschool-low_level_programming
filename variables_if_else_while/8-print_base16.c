678c5671fce23169528026002aaaca0648b155c3
Amjaadko <iamjaadk@gmail.com>
2025-08-28 18:55:16 +0000

    Add solution for task 8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-    int n;
+	int n;
 
-    for (n = 0; n < 16; n++)
-    {
-        if (n < 10)
-            putchar(n + '0');
-        else
-            putchar(n - 10 + 'a');
-    }
-    putchar('\n');
+	for (n = 0; n < 16; n++)
+	{
+		if (n < 10)
+			putchar(n + '0');
+		else
+			putchar(n - 10 + 'a');
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }

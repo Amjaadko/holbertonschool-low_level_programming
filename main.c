c7dedf52747f74f78dd8326c5916979d51babfab
Amjad Al Omani <iamjaadk@gmail.com>
2025-08-29 15:15:32 +0000

    Fix Betty style and add correct README

--- main.c
+++ main.c
@@ -1,23 +1,8 @@
-#include <unistd.h>
+#include "main.h"
 
-/* _putchar function */
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
-
-/* main function */
 int main(void)
 {
-    int i, j;
-    char letters[] = "abcdefghijklmnopqrstuvwxyz";
-
-    for (i = 0; i < 10; i++)
-    {
-        j = 0;
-        while (letters[j])
-            _putchar(letters[j++]);
-        _putchar('\n'); /* ثاني _putchar */
-    }
-    return 0;
+    print_alphabet_x10();
+    return (0);
 }
+

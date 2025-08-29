22276773146f41e88398788ae732fb6da0baf351
Amjad Al Omani <iamjaadk@gmail.com>
2025-08-29 11:45:16 +0000

    Add 2-print_alphabet_x10.c functionality using 2 _putchar

--- 2-print_alphabet_x10.c
+++ 2-print_alphabet_x10.c
@@ -1,15 +1,23 @@
 #include "main.h"
-																									/**
-* print_alphabet_x10 - prints 10 times the alphabet in lowercase
-* Return: void
-*/
-void print_alphabet_x10(void)	{
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
 	int i;
-	char ch;																							
+	char ch;
+
 	for (i = 0; i < 10; i++)
 	{
-		for (ch = 'a'; ch <= 'z'; ch++)
+		ch = 'a';
+		while (ch <= 'z')
 		{
-			_putchar(ch);																					}
+			_putchar(ch);
+			ch++;
+		}
 		_putchar('\n');
-	}																								}
+	}
+}

15ed08aec608c7a2bc86aa4d10d2900d72aeaaac
Amjad Al Omani <iamjaadk@gmail.com>
2025-08-29 11:34:13 +0000

    Add 2-print_alphabet_x10 functionality using 2 _putchar

--- main.c
+++ main.c
@@ -1,12 +1,23 @@
-#include <stdio.h>
+#include <unistd.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+/* _putchar function */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
+
+/* main function */
 int main(void)
 {
-    printf("Hello, Holberton!\n");
-    return (0);
+    int i, j;
+    char letters[] = "abcdefghijklmnopqrstuvwxyz";
+
+    for (i = 0; i < 10; i++)
+    {
+        j = 0;
+        while (letters[j])
+            _putchar(letters[j++]);
+        _putchar('\n'); /* ثاني _putchar */
+    }
+    return 0;
 }

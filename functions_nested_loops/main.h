d53c6e1964b36f0fbd7362bb248827293d73ad9e
Amjad Al Omani <iamjaadk@gmail.com>
2025-08-30 12:13:50 +0000

    Add solution for 5-sign task

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,12 +2,10 @@
 #define MAIN_H
 
 int _putchar(char c);
-/* _islower - checks for lowercase character */
 int _islower(int c);
-/* _isalpha - checks for alphabetic character */
 int _isalpha(int c);
-/* print_alphabet_x10 - prints alphabet 10 times */
 void print_alphabet_x10(void);
+int print_sign(int n);
 
-#endif /* MAIN_H */
+#endif
 

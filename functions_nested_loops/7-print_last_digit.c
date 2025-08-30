d01814148e0d57ae5b4cf2dbcccd79e8c51629e1
Amjad Al Omani <iamjaadk@gmail.com>
2025-08-30 12:28:04 +0000

    Add solutions for functions_nested_loops tasks 1 to 7 with Betty style

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,19 +2,19 @@
 
 /**
  * print_last_digit - prints the last digit of a number
- * @n: number to process
+ * @n: the number to check
  *
  * Return: value of the last digit
  */
 int print_last_digit(int n)
 {
-    int last;
+	int last;
 
-    last = n % 10;
-    if (last < 0)
-        last = -last;
-    _putchar('0' + last);
+	last = n % 10;
+	if (last < 0)
+		last *= -1;
+	_putchar(last + '0');
 
-    return (last);
+	return (last);
 }
 
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,12 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void print_alphabet_x10(void);
+int print_last_digit(int n);
 
 #endif
 

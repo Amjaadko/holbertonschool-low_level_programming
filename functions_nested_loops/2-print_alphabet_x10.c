4b23563d04858b37229fea78fbc67d6c60a175e0
Amjad Al Omani <iamjaadk@gmail.com>
2025-08-30 11:41:24 +0000

    Add solution for 2-print_alphabet_x10 task

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints 10 times the alphabet in lowercase
- *
- * Return: void
+ * print_alphabet_x10 - prints the alphabet 10 times in lowercase
  */
 void print_alphabet_x10(void)
 {
-    int i;
-    char c;
+	int i, j;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
 
-    for (i = 0; i < 10; i++)
-    {
-        for (c = 'a'; c <= 'z'; c++)
-        {
-            _putchar(c);
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 26; j++)
+			_putchar(alphabet[j]);
+		_putchar('\n');
+	}
 }
+

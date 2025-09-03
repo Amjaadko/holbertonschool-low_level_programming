54fae8e13357199bd9ddf54089b822e54662c5da
Amjad Alomani <iamjaadk@gmail.com>
2025-09-03 15:33:57 +0000

    Add Betty-compliant triangle printer and update main.h

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,8 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Standard library for printf if needed */
+#include <stdio.h>
+
+/* Function prototypes */
 int _putchar(char c);
-void print_square(int size);
+void print_triangle(int size);
 
 #endif /* MAIN_H */
 

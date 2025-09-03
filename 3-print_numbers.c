46a460ab625293abbc9364b69195c2edb2e7e62e
Amjad Alomani <iamjaadk@gmail.com>
2025-09-03 07:49:48 +0000

    Resolve merge conflicts

--- 3-print_numbers.c
+++ 3-print_numbers.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers from 0 to 9
+ * print_numbers - prints numbers 0 to 9
  *
  * Return: void
  */
 void print_numbers(void)
 {
-	int i;
+    int i;
 
-	for (i = 0; i <= 9; i++)
-	{
-		_putchar(i + '0');
-	}
-	_putchar('\n');
+    for (i = 0; i <= 9; i++)
+        _putchar(i + '0');
+    _putchar('\n');
 }
 

9e2f47960cebca318860ddf1bdb0604486633712
Amjad Alomani <iamjaadk@gmail.com>
2025-09-05 17:04:28 +0000

    Add 0-main.c for testing reset_to_98

--- 0-main.c
+++ 0-main.c
@@ -1,11 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 int main(void)
 {
     int n;
@@ -14,6 +9,6 @@ int main(void)
     printf("n=%d\n", n);
     reset_to_98(&n);
     printf("n=%d\n", n);
-    return (0);
+    return 0;
 }
 

4ed0d70fa2681c4f38ab20472d04086abdde8dc4
Amjad Al Omani <iamjaadk@gmail.com>
2025-09-06 12:56:48 +0000

    Add all pointer, array, and string functions

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -2,14 +2,13 @@
 
 /**
  * puts2 - prints every other character of a string
- * @str: string to print
+ * @str: the string to print
+ *
+ * Return: void
  */
 void puts2(char *str)
 {
 	int i;
-
-	if (!str)
-		return;
 
 	for (i = 0; str[i] != '\0'; i += 2)
 		_putchar(str[i]);

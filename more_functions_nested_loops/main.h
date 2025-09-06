3fb8b8a9b896a291e0847822f40742d1b4b3c829
Amjad Al Omani <iamjaadk@gmail.com>
2025-09-06 12:50:09 +0000

    Add _isdigit function and main.h to more_functions_nested_loops

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,12 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Standard library for printf if needed */
-#include <stdio.h>
-
-/* Function prototypes */
-int _putchar(char c);
-void print_triangle(int size);
+int _isdigit(int c);
 
 #endif /* MAIN_H */
 

2fa801cb33989f50e500528a76238af4f0fdf69e
Amjad Al Omani <iamjaadk@gmail.com>
2025-09-06 12:31:22 +0000

    Fix main.h and add all string/array functions

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,14 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* String manipulation functions */
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-
-/* Array manipulation functions */
 void reverse_array(int *a, int n);
 
-#endif /* MAIN_H_*
+#endif /* MAIN_H */
 

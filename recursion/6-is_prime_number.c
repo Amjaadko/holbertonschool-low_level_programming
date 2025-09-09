b52b144d266acda4212bdfc9ebc258332b5fd8be
Amjad Al Omani <iamjaadk@gmail.com>
2025-09-09 23:15:16 +0000

    Add recursion project files

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -2,28 +2,30 @@
 
 /**
  * is_prime_number - returns 1 if n is prime, 0 otherwise
- * @n: the number to check
+ * @n: number to check
  * Return: 1 if prime, 0 otherwise
  */
 int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (check_prime(n, n - 1));
+	if (n == 2)
+		return (1);
+	return (_is_prime(n, n - 1));
 }
 
 /**
- * check_prime - helper function to check prime recursively
- * @n: the number to check
+ * _is_prime - helper recursive function to check divisibility
+ * @n: number to check
  * @i: current divisor
  * Return: 1 if prime, 0 otherwise
  */
-int check_prime(int n, int i)
+int _is_prime(int n, int i)
 {
 	if (i == 1)
 		return (1);
 	if (n % i == 0)
 		return (0);
-	return (check_prime(n, i - 1));
+	return (_is_prime(n, i - 1));
 }
 

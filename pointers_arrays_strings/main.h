e4ebbf5ce7d17187fa2cc71d8e596c9d0ec50884
Amjad Al Omani <iamjaadk@gmail.com>
2025-09-09 21:53:00 +0000

    Fix merge conflicts in main.h and 3-main.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,14 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-<<<<<<< HEAD
 unsigned int _strspn(char *s, char *accept);
-=======
-char *_strchr(char *s, char c);
-int _putchar(char c);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
->>>>>>> 4977119b32836ec6e3d43d0b9ffe3ddfe973c260
 
 #endif /* MAIN_H */
 

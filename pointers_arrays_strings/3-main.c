7992d1aeedaf96d93d8511c702cab73bbaa45cc5
Amjad Al Omani <iamjaadk@gmail.com>
2025-09-09 21:55:45 +0000

    Fix merge conflicts in main.h and 3-main.c

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -1,8 +1,5 @@
 #include "main.h"
-<<<<<<< HEAD
 #include <stdio.h>
-=======
->>>>>>> 4977119b32836ec6e3d43d0b9ffe3ddfe973c260
 
 /**
  * main - check the code
@@ -11,7 +8,6 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
     char *s = "hello, world";
     char *f = "oleh";
     unsigned int n;
@@ -19,9 +15,5 @@ int main(void)
     n = _strspn(s, f);
     printf("%u\n", n);
     return (0);
-=======
-	print_numbers();
-	return (0);
->>>>>>> 4977119b32836ec6e3d43d0b9ffe3ddfe973c260
 }
 

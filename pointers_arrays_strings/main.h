4472bd2f5f68ab14797bb195599708edbc1fe0a6
Amjad Al Omani <iamjaadk@gmail.com>
2025-09-09 22:17:45 +0000

    Add 4-strpbrk.c with working main and print_rev, update main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,7 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Standard library headers */
+#include <stddef.h>
+
+/* Function prototypes */
+int _putchar(char c);
 unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+void print_rev(char *s);
+
+/* Add here prototypes for your other functions as needed */
 
 #endif /* MAIN_H */
 

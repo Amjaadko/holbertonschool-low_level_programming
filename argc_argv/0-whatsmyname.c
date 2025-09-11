b8e848316a31a6147f7de6a3cb93e9d4f759d1b3
Amjad Al Omani <iamjaadk@gmail.com>
2025-09-11 20:05:24 +0000

    Add argc_argv project - 0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the program name
+ * @argc: number of command line arguments
+ * @argv: array of strings containing the arguments
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+

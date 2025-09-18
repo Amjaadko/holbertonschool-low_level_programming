8346aeb5d663eeaf543d4ccd9352227672f2d4c9
Amjad Al Omani <iamjaadk@gmail.com>
2025-09-18 01:19:11 +0000

    Add Task 4 file

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,11 +1,31 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+
+/* helper function to calculate string length */
+int _strlen(char *s)
+{
+    int i = 0;
+    while (s[i])
+        i++;
+    return (i);
+}
+
+/* helper function to copy string manually */
+void _strcpy(char *dest, char *src)
+{
+    int i = 0;
+    while (src[i])
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+}
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
     dog_t *d;
+    int len_name, len_owner;
 
     if (!name || !owner)
         return (NULL);
@@ -14,22 +34,24 @@ dog_t *new_dog(char *name, float age, char *owner)
     if (!d)
         return (NULL);
 
-    d->name = malloc(strlen(name) + 1);
+    len_name = _strlen(name);
+    d->name = malloc(len_name + 1);
     if (!d->name)
     {
         free(d);
         return (NULL);
     }
-    strcpy(d->name, name);
+    _strcpy(d->name, name);
 
-    d->owner = malloc(strlen(owner) + 1);
+    len_owner = _strlen(owner);
+    d->owner = malloc(len_owner + 1);
     if (!d->owner)
     {
         free(d->name);
         free(d);
         return (NULL);
     }
-    strcpy(d->owner, owner);
+    _strcpy(d->owner, owner);
 
     d->age = age;
 

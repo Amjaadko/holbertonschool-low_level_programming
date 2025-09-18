00371198a95b541a559ade41cb214ac7a5431154
Amjad Al Omani <iamjaadk@gmail.com>
2025-09-18 01:01:30 +0000

    Add Task 0 file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,18 +2,27 @@
 #define DOG_H
 
 /**
- * struct dog - structure defining a dog
- * @name: name of the dog (string)
- * @age: age of the dog (float)
- * @owner: owner of the dog (string)
+ * struct dog - structure that defines a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: This struct stores basic information
+ * about a dog including its name, age, and owner.
  */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
+
+/* alias for struct dog */
 typedef struct dog dog_t;
+
+/* function prototypes */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif /* DOG_H */
 

a73a3a6b8670cac208a9dad5444e38c84768b16c
Amjad Al Omani <iamjaadk@gmail.com>
2025-09-18 00:52:55 +0000

    Add Task 0 file

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure defining a dog
+ * @name: name of the dog (string)
+ * @age: age of the dog (float)
+ * @owner: owner of the dog (string)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+typedef struct dog dog_t;
+
+#endif /* DOG_H */
+

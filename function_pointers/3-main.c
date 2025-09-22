8f912b3d664546b9cb382e5986c26b14c36d7da3
amjaadko <11950@holbertonstudents.com>
2025-09-22 06:29:15 +0000

    function_pointers: add required 3-main.c (override ignore)

---
+++ function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Simple calculator
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, exits with codes per spec on error
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+	int (*f)(int, int);
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	op = argv[2];
+	f = get_op_func(op);
+	if (f == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	if ((op[0] == '/' || op[0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = f(a, b);
+	printf("%d\n", result);
+	return (0);
+}

5c552129752ee9514604369d6f36d9788afd0d47
Amjad Alomani <iamjaadk@gmail.com>
2025-10-08 14:57:50 +0000

    upload files for task0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <unistd.h>
-#include <fcntl.h>
 #include <stdlib.h>
 
 /**
@@ -18,23 +16,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t rd, wr;
 	char *buffer;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
-	/* open file */
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
-	/* allocate buffer */
 	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	if (!buffer)
 	{
 		close(fd);
 		return (0);
 	}
 
-	/* read from file */
 	rd = read(fd, buffer, letters);
 	if (rd == -1)
 	{
@@ -43,17 +38,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	/* write to stdout */
 	wr = write(STDOUT_FILENO, buffer, rd);
-	if (wr == -1 || wr != rd)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
-
 	free(buffer);
 	close(fd);
+
+	if (wr == -1 || wr != rd)
+		return (0);
 
 	return (wr);
 }
